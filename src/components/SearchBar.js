d45d81083547d7a6655d0d11eedc909990f6ae28
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-04-03 19:59:06 +0100

    searchbar and semantic ui setup

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" />
+        </form>
+      </div>
+    )
+  }
+}
