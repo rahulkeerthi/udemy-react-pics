15ee2f6d89fc879bbe4a0b074c7dc12a1fcd82e4
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-04-03 21:06:40 +0100

    search bar made controlled

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
-import SearchBar from './SearchBar';
+import React, { Component } from "react";
+import SearchBar from "./SearchBar";
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <div>
-          <SearchBar />
-        </div>
-      </div>
-    );
-  };
-};
+	render() {
+		return (
+			<div className="ui container" style={{ marginTop: "10px" }}>
+				<SearchBar />
+			</div>
+		);
+	}
+}
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class SearchBar extends React.Component {
-  render() {
-    return (
-      <div>
-        <form>
-          <input type="text" />
-        </form>
-      </div>
-    )
-  }
+	state = { term: "" };
+
+	render() {
+		return (
+			<div className="ui segment">
+				<form className="ui form">
+					Image Search
+					<div className="field">
+						<input
+							type="text"
+							value={this.state.term}
+              placeholder="enter your query..."
+							onChange={(e) => this.setState({ term: e.target.value })}
+						/>
+					</div>
+				</form>
+			</div>
+		);
+	}
 }
